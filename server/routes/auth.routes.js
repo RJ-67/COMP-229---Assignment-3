3b0c130c93bb33071ddb5b8d3b2dc18f9a9882d2
RJ-67 <ronjoshua.c03@gmail.com>
2025-10-27 23:22:57 -0400

    Initial commit

---
+++ server/routes/auth.routes.js
@@ -0,0 +1,12 @@
+import express from "express";
+import authCtrl from "../controllers/auth.controller.js";
+const router = express.Router();
+router.route("/auth/signin").post(authCtrl.signin);
+router.route("/auth/signout").get(authCtrl.signout);
+router.get("/auth/protected", authCtrl.requireSignin, (req, res) => {
+  res.json({
+    message: "You are signed into a protected route!",
+    userId: req.auth._id,
+  });
+});
+export default router;
